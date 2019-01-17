e08c76d954c203372b8084a77b51d080730a9caf
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-17 15:17:28 +0000

    added functions for ui

--- src/app/components/pages/api-page/api-page.component.ts
+++ src/app/components/pages/api-page/api-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../services/api.service';
+import { ActivatedRoute } from '@angular/router';
+import { IApi } from '../../../interfaces/IApi';
 
 @Component({
   selector: 'app-api-page',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./api-page.component.scss']
 })
 export class ApiPageComponent implements OnInit {
-
-  constructor() { }
+  api: IApi;
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const apiId: string = this.route.snapshot.paramMap.get("id");
+    this.api = this.apiService.getApi(apiId);
+    console.log(this.api);
+  }
+
+  isStagingAvailiable = () => {
+    return this.api.hasOwnProperty('staging');
+  }
+
+  isProductionAvailiable = () => {
+    return this.api.hasOwnProperty('production');
+  }
+
+  checkIfApiIsCompliant = () => {
+    const values: boolean[] = Object.values(this.api.compliant);
+    return values.includes(false);
   }
 
 }
