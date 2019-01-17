39f90e8e2706b9e14ded7e0932ec6282a4e77676
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-17 15:03:14 +0000

    added url to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,9 @@ export const environment = {
     userPoolId: 'eu-west-2_rQ3dI3Vbp',
     region: 'eu-west-2',
     clientId: 'fjnkqnloe7adg1b85kvdmmscm'
+  },
+  apiURL : {
+    tokenService: ''
   }
 };
 
