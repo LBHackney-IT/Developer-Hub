26c1e823b308ed4e9867c48a19cb0c5c49cc3dc0
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-18 12:43:21 +0000

    updated api page

--- src/app/components/pages/api-page/api-page.component.ts
+++ src/app/components/pages/api-page/api-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../services/api.service';
 import { ActivatedRoute } from '@angular/router';
 import { IApi } from '../../../interfaces/IApi';
+import { HttpService } from '../../../services/http.service';
 
 @Component({
   selector: 'app-api-page',
@@ -10,15 +11,18 @@ import { IApi } from '../../../interfaces/IApi';
 })
 export class ApiPageComponent implements OnInit {
   api: IApi;
+  apiKey: string;
+  verified: boolean;
   constructor(
     private apiService: ApiService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private http: HttpService
   ) { }
 
   ngOnInit() {
     const apiId: string = this.route.snapshot.paramMap.get("id");
     this.api = this.apiService.getApi(apiId);
-    console.log(this.api);
+    this.getAPIKey();
   }
 
   isStagingAvailiable = () => {
@@ -34,4 +38,39 @@ export class ApiPageComponent implements OnInit {
     return values.includes(false);
   }
 
+  requestAPIKey = () => {
+    this.http.createApiKey(this.api.id)
+    .subscribe(
+      (response: Response) => {
+        // show in UI
+        this.getAPIKey();
+      },
+      (error) => {
+        console.log(error);
+      });
+  }
+
+  getAPIKey = () => {
+    this.http.readApiKey(this.api.id)
+    .subscribe(
+      (response: Response) => {
+        if(response) {
+          console.log("response", response);
+          this.apiKey = response['apiKey'];
+          this.verified = response['verified'];
+        }
+    },
+     (error) => {
+      console.log(error);
+     });
+  }
+
+  printApiKey = () => {
+    if(this.apiKey){
+      return this.apiKey + " <h1 class='govuk-tag govuk-tag--{{verified ? 'positive' : 'negative' }}'> {{verified ? 'Verified' : 'Not Verified' }} </h1>";
+    }
+    else {
+      return "Request Below";
+    }
+  }
 }
