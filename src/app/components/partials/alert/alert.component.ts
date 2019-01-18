ba542ff61b4c500058046e6c5cb8b5cab559cd71
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-18 15:53:31 +0000

    updated alert functionality

--- src/app/components/partials/alert/alert.component.ts
+++ src/app/components/partials/alert/alert.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { AlertService } from '../../../services/alert.service';
 
 @Component({
   selector: 'app-alert',
@@ -9,9 +10,25 @@ import { Subscription } from 'rxjs';
 export class AlertComponent implements OnInit {
   private subscription: Subscription;
   message: any;
-  constructor() { }
+  constructor(private alertService: AlertService) { }
 
   ngOnInit() {
+    this.subscription = this.alertService.getMessage().subscribe(message => { 
+        this.message = message; 
+    });
+
+    setTimeout(() => {
+      this.remove()
+    }, 10000);
+  }
+
+  remove = () => {
+    this.message = null;
+    this.subscription.unsubscribe();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
