f75a40ef1f58a79c41189759d83e5c80686e8a0e
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-18 12:40:46 +0000

    Added Auth Guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    const currentUser = this.authService.getCurrentUser();
+    if(currentUser){
+      return true;
+    }
+    this.router.navigateByUrl("/login");
+    return false;
+  }
+}
