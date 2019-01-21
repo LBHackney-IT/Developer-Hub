409bb027c4bcc7df6f2c494a7060e33638e090f4
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-21 14:37:17 +0000

    added new function and refactoredf

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -18,13 +18,18 @@ export class HttpService {
       cognito_username: cognitoUsername,
       api_id: apiId
     };
-    return this.httpClient.post(environment.apiURL.tokenService + 'create-api-key', payload);
+    return this.httpClient.post(environment.apiURL.tokenService + 'api-key', payload);
   }
 
    readApiKey = (apiId) => {
     const cognitoUsername: string = this.authService.getCognitoUsername();
     let params = new HttpParams().set('cognito_username', cognitoUsername).append("api_id", apiId);
-    return this.httpClient.get(environment.apiURL.tokenService + 'read-api-key', {params: params});
+    return this.httpClient.get(environment.apiURL.tokenService + 'api-key', {params: params});
+  }
+
+  readUserApiKeys = () => {
+    const cognitoUsername: string = this.authService.getCognitoUsername();
+    return this.httpClient.get(environment.apiURL.tokenService + 'api-key/' + cognitoUsername);
   }
 
   // verifyApiKey = (apiId) => {
