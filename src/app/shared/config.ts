a3949e9a55e8d12e66844665c4aed8c349158b5b
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-22 12:03:45 +0000

    created compliancy config map

---
+++ src/app/shared/config.ts
@@ -0,0 +1,14 @@
+export const compliancyConfigMap = {
+    revision_control: "One codebase tracked in revision control",
+    dependency_management: "Explicitly declare and isolate dependencies",
+    environment_config: "Store config in the environment",
+    decoupled_services: "Treat backing services as attached resources",
+    build_run_stage: "Strictly separate build and run stages",
+    stateless_process: "Execute the app as one or more stateless processes",
+    export_services: "Export services via port binding",
+    scalable_process: "Scale out via the process model",
+    rapid_start_shutdown: "Maximize robustness with fast startup and graceful shutdown",
+    maintain_consistency_between_stages: "Keep development, staging and production as similar as possible",
+    logging: "Treat logs as event streams",
+    admin_management_process: "Run admin/management asks as one-off processes"
+}
