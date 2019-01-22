c5557d33dab52adf77276b806acd9709c60388a3
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-22 12:04:51 +0000

    updated service to make http request

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { apis } from '../shared/mock-db';
 import { IApi } from '../interfaces/IApi';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
   
-  getListOfApis = () : IApi[] => {
-    return apis;
+  // getListOfApis = () : IApi[] => {
+  //   return apis;
+  // }
+
+  getListOfApis = () => {
+    return this.httpClient.get(environment.apiURL.apiService + 'api');
   }
 
 
@@ -19,6 +27,8 @@ export class ApiService {
         return api.id == id;
     });
   }
+
+  
 
   transformApiIntoDictionary = (apis: IApi[]) => {
     const transformedApis = apis.map((api) => {
