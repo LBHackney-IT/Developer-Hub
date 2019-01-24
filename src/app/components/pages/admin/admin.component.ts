cf25c2b627c2f5b992d1b97e5076e2a0b7e20a13
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-24 10:27:00 +0000

    added the ability to verify the user

--- src/app/components/pages/admin/admin.component.ts
+++ src/app/components/pages/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiKeyService } from '../../../services/apiKey.service';
+import { AuthService } from '../../../services/auth.service';
+import { AlertService } from '../../../services/alert.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  private tokenObjects: object[];
+
+  constructor(
+    private apiKeyService: ApiKeyService,
+    private authService: AuthService,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
+    this.getUnverifiedKeys();
+  }
+
+  verifyUser = (tokenObject) => {
+    const apiID: string = tokenObject.apiID;
+    const cognitoUsername: string = tokenObject.cognitoUsername;
+    this.apiKeyService.verifyApiKey(apiID, cognitoUsername)
+    .subscribe(
+      (response) => {
+        this.tokenObjects = this.tokenObjects.filter((item) => {
+          return item !== tokenObject;
+        });
+      },
+      (error) => {
+        console.log(error);
+      });
+  }
+
+  getUnverifiedKeys = async () => {
+    await this.apiKeyService.readAllUnverifiedApiKeys()
+    .subscribe(
+      (response) => {
+        this.tokenObjects = response['body'];
+      },
+      (error) => {
+        console.log(error);
+      });
   }
 
 }
