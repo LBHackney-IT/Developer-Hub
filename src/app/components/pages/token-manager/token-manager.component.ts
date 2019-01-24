de4e1b22ac5d9e348d7bef25af0dda3554227396
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-24 10:35:53 +0000

    changed the request api Key method

--- src/app/components/pages/token-manager/token-manager.component.ts
+++ src/app/components/pages/token-manager/token-manager.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiKeyService } from '../../../services/apiKey.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-token-manager',
@@ -9,25 +10,28 @@ import { ApiKeyService } from '../../../services/apiKey.service';
 export class TokenManagerComponent implements OnInit {
   private apiKeys;
   constructor(
-    private apiKeyService: ApiKeyService
-  ) { 
-    this.requestApiKeys();
+    private apiKeyService: ApiKeyService,
+    private authSerice: AuthService
+  ) {
+
   }
 
   ngOnInit() {
+    this.requestApiKeys();
   }
 
-  requestApiKeys = () => {
-    this.apiKeyService.readUserApiKeys()
+  requestApiKeys = async () => {
+    const cognitoUsername = this.authSerice.getCognitoUsername();
+    console.log('cognitoUsername', cognitoUsername);
+    await this.apiKeyService.readUserApiKeys()
     .subscribe(
       (response) => {
-        this.apiKeys = response
-        console.log(response);
+        this.apiKeys = response;
       },
       (error) => {
         console.log(error);
       }
-    )
+    );
   }
 
 }
