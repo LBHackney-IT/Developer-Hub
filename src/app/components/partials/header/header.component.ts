afb4eaf7bc00b7b7e43220819cee8bf967a4a173
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-24 10:35:25 +0000

    added new metjod to header

--- src/app/components/partials/header/header.component.ts
+++ src/app/components/partials/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  // private isUserLoggedIn: boolean;
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    // this.isUserLoggedIn = this.authService.isUserLoggedIn();
+    this.isUserLoggedIn();
+  }
+
+  isUserLoggedIn = (): boolean => {
+    const response = this.authService.isUserLoggedIn();
+    console.log(response);
+    return response;
   }
 
 }
