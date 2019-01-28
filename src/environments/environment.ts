38c4b7db9f96b85a6b7c250f6f125cc72600ec2b
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-01-28 16:11:22 +0000

    using live apis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,9 +9,13 @@ export const environment = {
     region: 'eu-west-2',
     clientId: 'fjnkqnloe7adg1b85kvdmmscm'
   },
+  // apiURL : {
+  //   tokenService: 'http://localhost:3000/',
+  //   apiService: 'http://localhost:3000/'
+  // }
   apiURL : {
-    tokenService: 'http://localhost:3000/',
-    apiService: 'http://localhost:3000/'
+    tokenService: 'https://j88uh2klte.execute-api.eu-west-2.amazonaws.com/dev/',
+    apiService: 'https://j88uh2klte.execute-api.eu-west-2.amazonaws.com/dev/'
   }
 };
 
