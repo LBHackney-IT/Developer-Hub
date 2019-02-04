bd45f0d7906b671704711b305860418b1cc90658
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-04 16:19:09 +0000

    created tab components

---
+++ src/app/components/partials/tabs/tab/tab.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.scss']
+})
+export class TabComponent implements OnInit {
+  @Input() label: string;
+  active: boolean;
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+
+
+
+}
---
+++ src/app/components/partials/tabs/tabs.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit,   ContentChildren, QueryList, AfterContentInit, ViewChild, ComponentFactoryResolver,
+  ViewContainerRef } from '@angular/core';
+import { TabComponent } from './tab/tab.component';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss']
+})
+export class TabsComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
+
+  constructor() { }
+
+  ngAfterContentInit() {
+    // get all active tabs
+    const activeTabs = this.tabs.filter((tab) => tab.active);
+
+    // if there is no active tab set, activate the first
+    if (activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+
+  }
+
+  public selectTab(tab: TabComponent) {
+    this.tabs.toArray().forEach((item) => {
+      item.active = false;
+    });
+    tab.active = true;
+  }
+
+}
