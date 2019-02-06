417cf0e089824da01fd134d24695abb4f804ded0
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-06 11:37:30 +0000

    added swagger endpoint page

---
+++ src/app/components/pages/swagger-endpoint-page/swagger-endpoint-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-swagger-endpoint-page',
+  templateUrl: './swagger-endpoint-page.component.html',
+  styleUrls: ['./swagger-endpoint-page.component.scss']
+})
+export class SwaggerEndpointPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
