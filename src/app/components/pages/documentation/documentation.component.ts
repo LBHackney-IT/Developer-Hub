dda99e1c6b08050911fcc376110b410ee3fd9d7a
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-06 10:00:31 +0000

    added documentation page

---
+++ src/app/components/pages/documentation/documentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import * as accordion from '../../../../../node_modules/govuk-frontend/components/accordion/accordion.js';
+@Component({
+  selector: 'app-documentation',
+  templateUrl: './documentation.component.html',
+  styleUrls: ['./documentation.component.scss']
+})
+export class DocumentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
