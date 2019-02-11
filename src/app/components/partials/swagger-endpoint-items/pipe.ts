8928e25fd3acde1b0987b2ffa7158002c32bf383
Daniele <daniele.tortora@hackney.gov.uk>
2019-02-11 11:34:21 +0000

    remove console.log from pipe

--- src/app/components/partials/swagger-endpoint-items/pipe.ts
+++ src/app/components/partials/swagger-endpoint-items/pipe.ts
@@ -5,11 +5,9 @@ import { ApiSearch } from '../../../services/apiSearch.service';
     name: 'SearchPipe'
 })
 export class SearchPipe implements PipeTransform {
-    
     constructor(private searchService: ApiSearch) { }
 
     transform = (apiData: any, formInput: string) => {
-        console.log(formInput)
         if (formInput && !this.isEmpty(formInput)) {
             return this.searchService.search(apiData, formInput);
         }
@@ -17,6 +15,6 @@ export class SearchPipe implements PipeTransform {
     }
 
     private isEmpty = (input: string) => {
-        return (input.replace(/\s/g, "").length > 0 ? false : true);
+        return (input.replace(/\s/g, '').length > 0 ? false : true);
     }
 }
