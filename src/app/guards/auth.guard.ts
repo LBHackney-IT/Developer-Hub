61b0234c5fbec67d72df317de9821f60ef3adcec
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-15 11:28:21 +0000

    cleaned up auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,7 +3,6 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { CognitoUser } from 'amazon-cognito-identity-js';
-import { IUser } from '../interfaces/IUser';
 
 /**
  *
@@ -25,7 +24,7 @@ export class AuthGuard implements CanActivate {
   constructor(
     private authService: AuthService,
     private router: Router
-  ) {}
+  ) { }
   /**
    *
    *
@@ -39,7 +38,6 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     const currentUser: CognitoUser = this.authService.getCurrentUser();
     const isUserLoggedIn: boolean = this.authService.isUserLoggedIn();
-    console.log(isUserLoggedIn);
     if (currentUser && isUserLoggedIn) {
       this.authService.refreshSession(currentUser);
       return true;
