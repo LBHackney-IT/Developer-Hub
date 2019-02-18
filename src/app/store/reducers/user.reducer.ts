be2053c7da3972dfbb052f1ce82d383e986ccbde
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-18 09:32:54 +0000

    created user store files

---
+++ src/app/store/reducers/user.reducer.ts
@@ -0,0 +1,29 @@
+import { Action } from '@ngrx/store';
+import { initialUserState, IUserState } from '../state/user.state';
+import { EUserActions, UserActions } from '../actions/user.actions';
+import { IUser } from '../../interfaces/IUser';
+
+export const userReducer = (state = initialUserState, action: UserActions): IUserState => {
+    switch (action.type) {
+        case EUserActions.SetUser: {
+            console.log('hi');
+
+            return {
+                ...state,
+                user: action.payload,
+                isAuthenticated: true
+            };
+        }
+        case EUserActions.RemoveUser: {
+            console.log('hello');
+            return {
+                ...state,
+                user: null,
+                isAuthenticated: false
+            };
+        }
+
+        default:
+            return state;
+    }
+};
