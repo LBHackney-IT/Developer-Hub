461e7b8513b753304835a5f5b75db46524223c93
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-20 11:15:03 +0000

    Added new api selector

--- src/app/store/selectors/api.selectors.ts
+++ src/app/store/selectors/api.selectors.ts
@@ -2,9 +2,14 @@ import { createSelector } from '@ngrx/store';
 import { IAppState } from '../state/app.state';
 import { IApiState } from '../state/api.state';
 
-const selectApi = (state: IAppState) => state.api;
+const selectApiState = (state: IAppState) => state.api;
 
 export const selectApiList = createSelector(
-    selectApi,
+    selectApiState,
     (state: IApiState) => state.apis
+);
+
+export const selectApi = createSelector(
+    selectApiState,
+    (state: IApiState, id) => state.apis.find(item => item.id === id)
 );
