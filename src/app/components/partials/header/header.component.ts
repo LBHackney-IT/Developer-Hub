fa3356004205d1bf33b7722e094220710e0859b2
Daniele <daniele.tortora@hackney.gov.uk>
2019-02-21 16:31:45 +0000

    add boolean

--- src/app/components/partials/header/header.component.ts
+++ src/app/components/partials/header/header.component.ts
@@ -2,13 +2,6 @@ import { Component, OnInit, OnChanges } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 
 
-/**
- *
- *
- * @export
- * @class HeaderComponent
- * @implements {OnInit}
- */
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -16,27 +9,14 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
-  /**
-   *
-   *
-   * @type {string}
-   * @memberof HeaderComponent
-   */
   public userName: string = null;
-  /**
-   *Creates an instance of HeaderComponent.
-   * @param {AuthService} authService
-   * @memberof HeaderComponent
-   */
+  private dropDown: boolean = false;
+  
   constructor(
     private authService: AuthService
   ) { }
 
-  /**
-   *
-   *
-   * @memberof HeaderComponent
-   */
+  
   ngOnInit() {
     if (this.isUserLoggedIn()) {
       this.userName = this.authService.getUserAttribute('name');
@@ -45,11 +25,7 @@ export class HeaderComponent implements OnInit {
     console.log(this.userName);
   }
 
-  /**
-   *
-   *
-   * @memberof HeaderComponent
-   */
+
   isUserLoggedIn = (): boolean => {
     const response = this.authService.isUserLoggedIn();
     console.log(response);
