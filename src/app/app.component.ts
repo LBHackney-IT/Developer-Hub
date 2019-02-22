67f7e532e8d088607c18b807e4554135a66d6585
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-22 12:41:17 +0000

    check if user is logged in the refreshes session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Developer-Hub-Frontend';
+  constructor(private authService: AuthService) {}
+  ngOnInit() {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser = () => {
+    const user = this.authService.getCurrentUser();
+    if (user !== null) {
+       this.authService.refreshSession(user);
+    }
+  }
 }
