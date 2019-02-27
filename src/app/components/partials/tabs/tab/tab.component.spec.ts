dab56fafd7807be3ac0175f42159c21d4e797f0f
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-27 11:07:10 +0000

    imported testing module

--- src/app/components/partials/tabs/tab/tab.component.spec.ts
+++ src/app/components/partials/tabs/tab/tab.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TabComponent } from './tab.component';
+import { TestingModule } from '../../../../../testing/utils';
 
 describe('TabComponent', () => {
   let component: TabComponent;
@@ -8,6 +9,9 @@ describe('TabComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        TestingModule
+      ],
       declarations: [ TabComponent ]
     })
     .compileComponents();
