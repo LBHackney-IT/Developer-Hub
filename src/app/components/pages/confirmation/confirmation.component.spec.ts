e55ca9c8aff557ec001d6e5f2569bfd99a21abf5
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-02-28 14:01:11 +0000

    added activated route mock

--- src/app/components/pages/confirmation/confirmation.component.spec.ts
+++ src/app/components/pages/confirmation/confirmation.component.spec.ts
@@ -1,22 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfirmationComponent } from './confirmation.component';
+import { ActivatedRoute } from '@angular/router';
+import { MockActivatedRoute } from '../../../../testing/utils';
 
 describe('ConfirmationComponent', () => {
   let component: ConfirmationComponent;
   let fixture: ComponentFixture<ConfirmationComponent>;
+  let activatedRoute: MockActivatedRoute;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConfirmationComponent ]
+      declarations: [ConfirmationComponent],
+      providers: [{ provide: ActivatedRoute, useClass: MockActivatedRoute }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ConfirmationComponent);
     component = fixture.componentInstance;
+
+    activatedRoute = fixture.debugElement.injector.get(ActivatedRoute) as any;
+    activatedRoute.testParamMap = { type: 'forgot-password' };
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
   it('should create', () => {
