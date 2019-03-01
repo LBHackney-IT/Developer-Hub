282b1d5084446b8722ef2342c0953216b31b268f
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-03-01 14:27:29 +0000

    removed optional

--- src/app/validators/check-password.validator.ts
+++ src/app/validators/check-password.validator.ts
@@ -1,10 +1,4 @@
 import { FormGroup, ValidatorFn, AbstractControl } from '@angular/forms';
-// export function checkPassword(password: string): ValidatorFn {
-//     return (control: AbstractControl): { [key: string]: any } | null => {
-//         const confirmPassword = control.v;
-//         return password === confirmPassword ? null : {'passwordsDoNotMatch': 'Passwords Do Not Much'};
-//     };
-// }
 
 export function checkPassword(control: AbstractControl) {
     const password = control.get('password').value;
