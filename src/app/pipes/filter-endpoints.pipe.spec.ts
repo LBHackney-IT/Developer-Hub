8de7ada5d39469f83097b9fcb6258e8f75ba3012
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-03-04 10:56:45 +0000

    fixed tests

--- src/app/pipes/filter-endpoints.pipe.spec.ts
+++ src/app/pipes/filter-endpoints.pipe.spec.ts
@@ -1,8 +1,10 @@
 import { FilterEndpointsPipe } from './filter-endpoints.pipe';
+import { ApiSearch } from '../services/apiSearch.service';
 
 describe('FilterEndpointsPipe', () => {
   it('create an instance', () => {
-    const pipe = new FilterEndpointsPipe();
+    const searchService: ApiSearch = new ApiSearch();
+    const pipe = new FilterEndpointsPipe(searchService);
     expect(pipe).toBeTruthy();
   });
 });
