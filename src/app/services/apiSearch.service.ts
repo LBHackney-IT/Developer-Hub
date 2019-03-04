71e53f1c3dc97d55a6c7270b5fa2a1c38ddc8d21
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-03-04 14:22:58 +0000

    refactored

--- src/app/services/apiSearch.service.ts
+++ src/app/services/apiSearch.service.ts
@@ -10,59 +10,35 @@ export class ApiSearch {
     search = (swaggerEndpoints: ISwagger[], searchText: string) => {
         const words = searchText.toLowerCase().split(' ');
         this.swaggerEndpoints = swaggerEndpoints;
-        // words.forEach(word => {
-        //     this.filteredSwaggerEndpoints = this.getMatchingEndpoints(word);
-        // });
-        this.filteredSwaggerEndpoints = this.getMatchingEndpoints(searchText);
-
-        console.log(this.filteredSwaggerEndpoints);
+        words.forEach(word => {
+            this.filteredSwaggerEndpoints = this.getMatchingEndpoints(word);
+        });
 
         return this.filteredSwaggerEndpoints;
     }
 
-
     private getMatchingEndpoints = (word: string) => {
         const swaggerEndpoints = this.swaggerEndpoints.filter((swaggerEndpoint) => {
-            swaggerEndpoint.paths =  swaggerEndpoint.paths.filter((path) => {
+            const paths = swaggerEndpoint.paths.filter((path) => {
                 // Check if path summary exists
+                let pass = false;
                 if (path.summary) {
-                    return path.tags[0].toLowerCase().includes(word) ||
-                    path.url.includes(word) ||
-                    path.summary.includes(word);
-                } else {
-                    return path.tags[0].toLowerCase().includes(word) ||
-                    path.url.includes(word);
+                    pass =  path.summary.includes(word) ? true : pass;
                 }
 
+                if (path.tags[0]) {
+                    pass =  path.tags[0].includes(word) ? true : pass;
+                }
+
+                if (path.url[0]) {
+                    pass =  path.url.includes(word) ? true : pass;
+                }
+                return pass;
             });
-
-            return swaggerEndpoint.paths.length > 0;
-
+            return paths.length > 0;
         });
         return swaggerEndpoints;
     }
-
-
-    // private getMatchingEndpoints = (word: string) => {
-    //     for (var i=0; i<this.apiData.length; i++) {
-    //         let matchingEndpoints = this.apiData[i].paths.filter(function (element: any) {
-    //             return element.tags.toString().toLowerCase().indexOf(word) > -1 ||
-    //                 element.url.toLowerCase().indexOf(word) > -1 ||
-    //                 element.summary.toLowerCase().indexOf(word) > -1;
-    //         })
-    //         this.resultApiData[i].paths = matchingEndpoints;
-    //     }
-    // }
-
-
-    // private populateLocalApiParentData = () => {
-    //     this.apiData.forEach(api => {
-    //         let emptyApi = new ApiModel;
-    //         emptyApi.title = api.title;
-    //         emptyApi.paths = [];
-    //         this.resultApiData.push(emptyApi);
-    //     });
-    // }
 }
 
 export class ApiModel {
