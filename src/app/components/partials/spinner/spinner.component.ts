cdc40ab2f5ced7b355a01ae4268722193fbeb620
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-03-06 15:41:26 +0000

    updated spinner logic

--- src/app/components/partials/spinner/spinner.component.ts
+++ src/app/components/partials/spinner/spinner.component.ts
@@ -1,13 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SpinnerService } from 'src/app/services/spinner.service';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss']
 })
-export class SpinnerComponent implements OnInit {
+export class SpinnerComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  private isLoading = false;
 
-  constructor() {}
+  constructor(private spinnerService: SpinnerService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscription = this.spinnerService.getSpinner().subscribe((result) => {
+      console.log('init');
+      console.log(result.display);
+      this.isLoading = result.display;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/services/spinner.service.ts
+++ src/app/services/spinner.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpinnerService {
+  private subject: Subject<any> =  new Subject<any>();
+  constructor() {
+  }
 
-  constructor() { }
+  displaySpinner = () => {
+    this.subject.next({display: true});
+  }
+
+  hideSpinner = () => {
+    console.log('hide');
+    this.subject.next({display: false});
+  }
+
+  getSpinner(): Observable<any> {
+    return this.subject.asObservable();
+  }
 }
