6b2e66bce81d8354707b269a919fa79f5addeddc
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-03-06 13:49:25 +0000

    Created test for sorting

--- src/app/pipes/order-apis-alphabetically.pipe.spec.ts
+++ src/app/pipes/order-apis-alphabetically.pipe.spec.ts
@@ -1,8 +1,19 @@
 import { OrderApisAlphabeticallyPipe } from './order-apis-alphabetically.pipe';
+import { generateTestApis } from '../../testing/mock-db';
 
 describe('OrderApisAlphabeticallyPipe', () => {
+  let pipe: OrderApisAlphabeticallyPipe;
+  beforeEach(() => {
+    pipe = new OrderApisAlphabeticallyPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new OrderApisAlphabeticallyPipe();
+    // const pipe = new OrderApisAlphabeticallyPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should sort title alphabetically', () => {
+    const result = pipe.transform(generateTestApis(4));
+    expect(result[0].title.charCodeAt(0)).toBeLessThan(result[3].title.charCodeAt(0));
   });
 });
