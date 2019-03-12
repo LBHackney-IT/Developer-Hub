04466415fd4a6275df97aecacd5972d5b43964db
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-03-12 14:35:51 +0000

    altered effects

--- src/app/store/effects/api.effects.ts
+++ src/app/store/effects/api.effects.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {  of, forkJoin} from 'rxjs';
+import { of, forkJoin } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { EApiActions, GetApiList, GetApiListSuccess, DeleteApi, DeleteApiSuccess } from '../actions/api.actions';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, concatMap } from 'rxjs/operators';
 import { IAppState } from '../state/app.state';
 import { IApi } from 'src/app/interfaces/IApi';
 import { ApiService } from '../../services/api.service';
@@ -13,25 +13,21 @@ export class ApiEffects {
 
     constructor(
         private actions$: Actions,
-        private store$: Store<IAppState>,
         private apiService: ApiService
-    ) {}
+    ) { }
 
     @Effect()
     getApiList$ =
-    this.actions$.pipe(
-        ofType<GetApiList>(EApiActions.GetApiList),
-        switchMap(() => this.apiService.getListOfApis()),
-        switchMap((response: IApi[]) => of( new GetApiListSuccess(response)))
-    );
+        this.actions$.pipe(
+            ofType<GetApiList>(EApiActions.GetApiList),
+            switchMap(() => this.apiService.getListOfApis()),
+            switchMap((response: IApi[]) => of(new GetApiListSuccess(response)))
+        );
 
     @Effect()
     deleteApi$ =
-    this.actions$.pipe(
-        ofType<DeleteApi>(EApiActions.DeleteApi),
-        switchMap(
-            (action: DeleteApi) => (
-            forkJoin(this.apiService.deleteApi(action.payload),
-            of( new DeleteApiSuccess(action.payload)))
-    )));
+        this.actions$.pipe(
+            ofType<DeleteApiSuccess>(EApiActions.DeleteApiSuccess),
+            switchMap((action: DeleteApiSuccess) => this.apiService.deleteApi(action.payload))
+        );
 }
