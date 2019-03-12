5f4a3100d9870ab6bbf1abf915ed7d34e3721e01
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-03-12 13:25:07 +0000

    added api Services test for put Api

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed, getTestBed } from '@angular/core/testing';
 import { ApiService } from './api.service';
 import { TestingModule } from '../../testing/utils';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { generateTestApis } from '../../testing/mock-db';
+import { IApi } from '../interfaces/IApi';
 
 describe('ApiService', () => {
   let injector: TestBed;
@@ -28,11 +30,28 @@ describe('ApiService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('deleteApi', () => {
-    let id: string;
-    beforeEach(() => {
-      id = 'test_api';
+  describe('putApi', () => {
+    const api: IApi = generateTestApis(1)[0];
+
+    it('should strip the body key from the response', () => {
+      service.putApi(api).subscribe((response) => {
+        expect(response.hasOwnProperty('body')).toBeFalsy();
+      });
+      const mockReq = httpMock.expectOne(`${service.url}api`);
+
     });
+
+    it('should call the Post method', () => {
+      service.putApi(api).subscribe((response) => {
+      });
+      const mockReq = httpMock.expectOne(`${service.url}api`);
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.method.toUpperCase()).toBe('POST');
+    });
+  });
+
+  describe('deleteApi', () => {
+    const id = 'test_api';
 
     it('should strip the body key from the response', () => {
       service.deleteApi(id).subscribe((response) => {
@@ -42,14 +61,13 @@ describe('ApiService', () => {
 
     });
 
-    it('should call the Deete method', () => {
+    it('should call the Delete method', () => {
       service.deleteApi(id).subscribe((response) => {
       });
       const mockReq = httpMock.expectOne(`${service.url}api/${id}`);
       expect(mockReq.cancelled).toBeFalsy();
       expect(mockReq.request.method.toUpperCase()).toBe('DELETE');
     });
-
   });
 
 });
