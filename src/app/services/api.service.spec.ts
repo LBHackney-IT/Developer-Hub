31004cf6f37ff263f0850292e352f4b985e1c84d
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-03-12 12:57:36 +0000

    added new test

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,17 +1,55 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, getTestBed } from '@angular/core/testing';
 import { ApiService } from './api.service';
 import { TestingModule } from '../../testing/utils';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      TestingModule
-    ],
-  }));
+  let injector: TestBed;
+  let service: ApiService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        TestingModule
+      ],
+    });
+
+    injector = getTestBed();
+    service = injector.get(ApiService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should be created', () => {
-    const service: ApiService = TestBed.get(ApiService);
     expect(service).toBeTruthy();
   });
+
+  describe('deleteApi', () => {
+    let id: string;
+    beforeEach(() => {
+      id = 'test_api';
+    });
+
+    it('should strip the body key from the response', () => {
+      service.deleteApi(id).subscribe((response) => {
+        expect(response.hasOwnProperty('body')).toBeFalsy();
+      });
+      const mockReq = httpMock.expectOne(`${service.url}api/${id}`);
+
+    });
+
+    it('should call the Deete method', () => {
+      service.deleteApi(id).subscribe((response) => {
+      });
+      const mockReq = httpMock.expectOne(`${service.url}api/${id}`);
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.method.toUpperCase()).toBe('DELETE');
+    });
+
+  });
+
 });
