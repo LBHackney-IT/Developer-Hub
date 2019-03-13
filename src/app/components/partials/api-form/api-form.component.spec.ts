17f44f849816ad728c4038a411c30554d3db6c13
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-03-13 16:55:35 +0000

    added tests to api form

--- src/app/components/partials/api-form/api-form.component.spec.ts
+++ src/app/components/partials/api-form/api-form.component.spec.ts
@@ -12,9 +12,9 @@ describe('ApiFormComponent', () => {
       imports: [
         TestingModule
       ],
-      declarations: [ ApiFormComponent ]
+      declarations: [ApiFormComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -26,4 +26,214 @@ describe('ApiFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create a form with twelve controls', () => {
+    expect(Object.keys(component.apiForm.controls).length).toBe(12);
+    expect(component.apiForm.contains('id')).toBe(true);
+    expect(component.apiForm.contains('title')).toBe(true);
+    expect(component.apiForm.contains('summary')).toBe(true);
+    expect(component.apiForm.contains('description')).toBe(true);
+    expect(component.apiForm.contains('compliant')).toBe(true);
+    expect(component.apiForm.contains('internal')).toBe(true);
+    expect(component.apiForm.contains('staging')).toBe(true);
+    expect(component.apiForm.contains('approved')).toBe(true);
+    expect(component.apiForm.contains('stage')).toBe(true);
+    expect(component.apiForm.contains('github_url')).toBe(true);
+    expect(component.apiForm.contains('owner')).toBe(true);
+  });
+
+  describe('title', () => {
+    it('should make the title control required', () => {
+      const control = component.apiForm.get('title');
+      control.setValue('');
+      expect(control.valid).toBe(false);
+    });
+
+    it('should be false if title is less than 6', () => {
+      const control = component.apiForm.get('title');
+      control.setValue('a'.repeat(5));
+      expect(control.valid).toBe(false);
+    });
+
+    it('should be true if title is >= than 6', () => {
+      const control = component.apiForm.get('title');
+      control.setValue('a'.repeat(6));
+      expect(control.valid).toBe(true);
+    });
+  });
+
+  describe('summary', () => {
+    it('should make the summary control required', () => {
+      const control = component.apiForm.get('summary');
+      control.setValue('');
+      expect(control.valid).toBe(false);
+    });
+
+    it('should be false if summary is less than 6', () => {
+      const control = component.apiForm.get('summary');
+      control.setValue('a'.repeat(5));
+      expect(control.valid).toBe(false);
+    });
+
+    it('should be true if summary is >= than 6', () => {
+      const control = component.apiForm.get('summary');
+      control.setValue('a'.repeat(6));
+      expect(control.valid).toBe(true);
+    });
+  });
+
+  describe('description', () => {
+    it('should make the description control required', () => {
+      const control = component.apiForm.get('description');
+      control.setValue('');
+      expect(control.valid).toBe(false);
+    });
+
+    it('should be false if description is less than 6', () => {
+      const control = component.apiForm.get('description');
+      control.setValue('a'.repeat(5));
+      expect(control.valid).toBe(false);
+    });
+
+    it('should be true if description is >= than 6', () => {
+      const control = component.apiForm.get('description');
+      control.setValue('a'.repeat(6));
+      expect(control.valid).toBe(true);
+    });
+  });
+
+  describe('internal', () => {
+    it('should make the internal control required', () => {
+      const control = component.apiForm.get('internal');
+      control.setValue(null);
+      expect(control.valid).toBe(false);
+    });
+  });
+
+  describe('staging', () => {
+    describe('url', () => {
+      it('should make the url control required', () => {
+        const control = component.apiForm.get('staging.url');
+        control.setValue('');
+        expect(control.valid).toBe(false);
+      });
+      it('should be false if summary is less than 3', () => {
+        const control = component.apiForm.get('staging.url');
+        control.setValue('a'.repeat(2));
+        expect(control.valid).toBe(false);
+      });
+      it('should be true if summary is >= than 3', () => {
+        const control = component.apiForm.get('staging.url');
+        control.setValue('a'.repeat(3));
+        expect(control.valid).toBe(true);
+      });
+    });
+
+    describe('swagger_url', () => {
+      it('should make the swagger_url control required', () => {
+        const control = component.apiForm.get('staging.swagger_url');
+        control.setValue('');
+        expect(control.valid).toBe(false);
+      });
+      it('should be false if summary is less than 3', () => {
+        const control = component.apiForm.get('staging.swagger_url');
+        control.setValue('a'.repeat(2));
+        expect(control.valid).toBe(false);
+      });
+      it('should be true if summary is >= than 3', () => {
+        const control = component.apiForm.get('staging.swagger_url');
+        control.setValue('a'.repeat(3));
+        expect(control.valid).toBe(true);
+      });
+    });
+
+    describe('deployed', () => {
+      it('should make the internal control required', () => {
+        const control = component.apiForm.get('staging.deployed');
+        // control.setValue(null);
+        console.log(control);
+        expect(control.valid).toBe(false);
+      });
+    });
+  });
+
+  describe('production', () => {
+    describe('url', () => {
+      it('should make the url control required', () => {
+        const control = component.apiForm.get('production.url');
+        control.setValue('');
+        expect(control.valid).toBe(false);
+      });
+      it('should be false if summary is less than 3', () => {
+        const control = component.apiForm.get('production.url');
+        control.setValue('a'.repeat(2));
+        expect(control.valid).toBe(false);
+      });
+      it('should be true if summary is >= than 3', () => {
+        const control = component.apiForm.get('production.url');
+        control.setValue('a'.repeat(3));
+        expect(control.valid).toBe(true);
+      });
+    });
+
+    describe('swagger_url', () => {
+      it('should make the swagger_url control required', () => {
+        const control = component.apiForm.get('production.swagger_url');
+        control.setValue('');
+        expect(control.valid).toBe(false);
+      });
+      it('should be false if summary is less than 3', () => {
+        const control = component.apiForm.get('production.swagger_url');
+        control.setValue('a'.repeat(2));
+        expect(control.valid).toBe(false);
+      });
+      it('should be true if summary is >= than 3', () => {
+        const control = component.apiForm.get('production.swagger_url');
+        control.setValue('a'.repeat(3));
+        expect(control.valid).toBe(true);
+      });
+    });
+
+    describe('deployed', () => {
+      it('should make the internal control required', () => {
+        const control = component.apiForm.get('production.deployed');
+        control.setValue(null);
+        expect(control.valid).toBe(false);
+      });
+    });
+  });
+
+  describe('approved', () => {
+    it('approved should be false', () => {
+      const control = component.apiForm.get('approved');
+      expect(control.value).toBe(false);
+    });
+  });
+
+  describe('stage', () => {
+    it('should make the stage control required', () => {
+      const control = component.apiForm.get('stage');
+      expect(control.valid).toBe(false);
+    });
+  });
+
+  describe('github_url', () => {
+    it('should make the github_url control required', () => {
+      const control = component.apiForm.get('github_url');
+      control.setValue('');
+      expect(control.valid).toBe(false);
+    });
+    it('should be false if summary is less than 6', () => {
+      const control = component.apiForm.get('github_url');
+      control.setValue('a'.repeat(5));
+      expect(control.valid).toBe(false);
+    });
+    it('should be true if summary is >= than 6', () => {
+      const control = component.apiForm.get('github_url');
+      control.setValue('a'.repeat(6));
+      expect(control.valid).toBe(true);
+    });
+  });
+
+
 });
