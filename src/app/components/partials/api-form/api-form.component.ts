beaa1f9ee572f713a811136e47b3dd7bb05cf647
Thomas Lawanson <thomas.lawanson@hackney.gov.uk>
2019-05-31 16:08:08 +0100

    added development and id  to stages

--- src/app/components/partials/api-form/api-form.component.ts
+++ src/app/components/partials/api-form/api-form.component.ts
@@ -95,6 +95,9 @@ export class ApiFormComponent implements OnInit {
       Validators.required
     ]),
     staging: new FormGroup({
+      id: new FormControl('staging', [
+        Validators.required,
+      ]),
       url: new FormControl('', [
         Validators.required,
         Validators.minLength(3),
@@ -111,6 +114,9 @@ export class ApiFormComponent implements OnInit {
       ])
     }),
     development: new FormGroup({
+      id: new FormControl('development', [
+        Validators.required,
+      ]),
       url: new FormControl('', [
         Validators.required,
         Validators.minLength(3),
@@ -127,6 +133,9 @@ export class ApiFormComponent implements OnInit {
       ])
     }),
     production: new FormGroup({
+      id: new FormControl('production', [
+        Validators.required,
+      ]),
       url: new FormControl('', [
         Validators.required,
         Validators.minLength(3)
@@ -264,7 +273,7 @@ export class ApiFormComponent implements OnInit {
       internal: this.api.internal,
       staging: this.api.staging,
       production: this.api.production,
-      // development: this.api.development,
+      development: this.api.development,
       description: this.api.description,
       approved: this.api.approved,
       stage: this.api.stage,
